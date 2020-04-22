e8ea93f489f52a8db04ca63d4517926993dfd2af
Daniel Jordan <danieljordan86@gmail.com>
2020-04-22 14:33:54 -0600

    Toggle search view

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <template>
   <v-app>
     <v-container>
-      <Search />
-      <MovieList
-        :movies="movies"
-        @add-to-favorites="addFavorite($event)"
-        @remove-from-list="removeMovie($event)"
-      />
-    </v-container>
-    <v-container>
-      <h2>Favorites</h2>
-      <MovieList :movies="favorites" />
-    </v-container>
+        <Toolbar />
+        <h4>Top-Rated Films ({{ movies.length }})</h4>
+        <MovieList
+          :movies="movies"
+          @add-to-favorites="addFavorite($event)"
+          @remove-from-list="removeMovie($event)"
+        />
+        <h2>Favorites ({{ favorites.length }})</h2>
+        <MovieList
+          :movies="favorites"
+          @remove-from-list="removeFromFavorites($event)"
+        />
+      </v-container>
   </v-app>
 </template>
 
 <script>
 import axios from 'axios';
+import Toolbar from './components/Toolbar.vue';
 import MovieList from './components/MovieList.vue';
-import Search from './components/Search.vue';
 
 export default {
   name: 'App',
   components: {
+    Toolbar,
     MovieList,
-    Search,
   },
 
   data() {
@@ -41,6 +43,9 @@ export default {
     },
     removeMovie(id) {
       this.movies = this.movies.filter((el) => el.id !== id);
+    },
+    removeFromFavorites(id) {
+      this.favorites = this.favorites.filter((el) => el.id !== id);
     },
   },
 
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <v-toolbar dense>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>MovieApp</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon @click="showSearch = !showSearch">
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <Search v-show="showSearch"/>
+  </v-container>
+</template>
+
+<script>
+import Search from './Search.vue';
+
+export default {
+  name: 'Toolbar',
+  components: {
+    Search,
+  },
+  data() {
+    return {
+      showSearch: false,
+    };
+  },
+};
+</script>
