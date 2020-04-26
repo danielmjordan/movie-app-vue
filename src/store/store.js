d3514004e44bff1c78a82bc9d9019119903e1fde
Daniel Jordan <danieljordan86@gmail.com>
2020-04-26 11:21:33 -0600

    Configure store for vuex

--- src/store/store.js
+++ src/store/store.js
@@ -2,3 +2,15 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    movies: null,
+    favorites: null,
+    showFavorites: false,
+    heading: '',
+    page: 1,
+  },
+});
+
+export default store;
